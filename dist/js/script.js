24e6aa7ea914bdff6447975b3444e97a11b7d82f
Dalbayob <shohtod@gmail.com>
2021-09-06 19:09:23 +0500

    mod: changed and optimized the page

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		delay: 200, // values from 0 to 3000, with step 50ms
 		duration: 700, // values from 0 to 3000, with step 50ms
 		easing: 'ease', // default easing for AOS animations
-		once: true, // whether animation should happen only once - while scrolling down
+		once: false, // whether animation should happen only once - while scrolling down
 		anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
 	});
 	// Hamburger
